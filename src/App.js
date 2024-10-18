247ec873762602f055819e47f76b76a7c0c92f75
Sejoqariz <sejokarizz@gmail.com>
2024-10-18 09:13:00 +0000

    create a cocktailpage card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home.jsx';
+import CocktailDetails from './CocktailDetails.jsx';
 
 
-
-function App() {
+const App = () => {
   return (
-    <div>cocktailReacted</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cocktail/:cocktailId" element={<CocktailDetails />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
